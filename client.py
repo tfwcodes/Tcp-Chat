daa2ca734a4be148ac07be8e600159e3872a74b7
tfwcodes <86290437+tfwcodes@users.noreply.github.com>
2021-08-06 00:18:19 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 import socket
 import threading
+from time import sleep
 
 nicknames = []
 clients = []
+
+
 
 def receiver():
     host = input(str("Enter the host to connect: "))
@@ -17,14 +20,36 @@ def receiver():
     s.connect((host, port))
     s.send(nickname2.encode())
 
+
     nickname = s.recv(BuffSize).decode()
     nicknames.append(nickname)
 
+    def brodcast(message):
+        for client in clients:
+            s.send(message)
+
     print(f"Connection from: {host_ip} with the nickname: {nickname}")
     while 1:
-        print(f"{nickname}: {s.recv(BuffSize).decode()}")
-        messsage = input("Enter a message: ")
-        s.send(messsage.encode())
+        try:
+            x = f"{nickname}: {s.recv(BuffSize).decode()}"
+            if x == "admin: ?kick {nickname2}":
+                try:
+                    print("You were kicked by an admin")
+                    sleep(2)
+                    s.close()
+                    exit()
+                except socket.error:
+                    print("You were kicked by an admin")
+ 
+            else:
+                try:
+                    print(x)
+                    messsage = input("Enter a message: ")
+                    brodcast(messsage   .encode())
+                except socket.error:
+                    print("You were kicked by an admin")
+        except socket.error:
+            print("You were kicked by an admin")
 
 t = threading.Thread(target=receiver)
 t.start()
